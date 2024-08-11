f085f978b9af8d4e1ab7e8df549746c3c48f64c4
Alp-Kayfeci <alpmuratkayfeci@gmail.com>
2024-08-11 17:20:55 +0300

    aa

--- operitaons.cs
+++ operitaons.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,34 @@ namespace WindowsFormsApp7
 {
     internal static class operitaons
     {
+        private static string connectionString = "Server=localhost;Database=radyo_db;User=root;Password=";
+        private static MySqlConnection connection = new MySqlConnection(connectionString);
+
+        public static List<Radyo> getRadyoURL()
+        {
+            List<Radyo> list = new List<Radyo>();
+            string query = "select * FROM  radyolar";
+            connection.Open();
+            MySqlCommand command =  new MySqlCommand(query, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            connection.Close();
+            while (reader.Read())
+            {
+                Radyo radyo = new Radyo();
+                radyo.Id = reader.GetInt32("id");
+                radyo.RadyoAdi = reader.GetString("radyo_adi");
+                radyo.RadyoUrl = reader.GetString("radyo_url");
+                list.Add(radyo);
+            }
+            return list;
+        }
+
+
+
+
 
 
     }
+
+
 }
